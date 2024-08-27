28bc450f9490662fffa75df10150545173518ba3
Rupam <rupammondal564@gmail.com>
2024-08-27 07:43:23 +0530

    Lazy loading added

--- src/Components/Routes/Routing.jsx
+++ src/Components/Routes/Routing.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom"
-import Coindetails from "../Pages/Coindetails"
-import Home from "../Pages/Home"
+import { lazy , Suspense } from "react"
+
+const Home = lazy(() => import('../Pages/Home'))
+const Coindetails = lazy(() => import('../Pages/Coindetails'))
+
 
 function Routing(){
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="/details/:coinID" element={<Coindetails />}></Route>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/details/:coinID" element={<Coindetails />}></Route>
+                </Routes>
+            </Suspense>
         </>
     )
 }
